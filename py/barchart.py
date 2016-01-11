752a98b34f6e1b11e5e90817b5c1282d31b297cb
AlexanderLuke <luke.j.alexander1@gmail.com>
2016-01-10 20:55:00 -0500

    Implementing update.py as a method in Barchart.py.

--- py/barchart.py
+++ py/barchart.py
@@ -8,6 +8,40 @@ import csv
 import datetime
 import math
 
+def update(csvfile):
+    r = csv.reader(open(csvfile,"r"))
+    final_rows = []
+    first_row = r.next()
+    final_rows.append(first_row)
+
+    date_row = r.next()
+    date_row.append(time + " Price")
+    date_row.append(time + " Profit")
+    final_rows.append(date_row)
+
+    difference_list = []
+
+    for item in r:
+        stock = item[0]
+        open_price = float(item[1])
+        if Share(stock).get_price() is None:
+            profit_row = item
+            profit_row.append(time + " Net Profit")
+            profit_row.append(float("{0:.2f}".format(sum(difference_list))))
+            final_rows.append(profit_row)
+            break
+        else:
+            new_price = float(Share(stock).get_price())
+            difference = float("{0:.2f}".format(new_price - open_price))
+            difference_list.append(difference)
+            item.append(new_price)
+            item.append(difference)
+            final_rows.append(item)
+
+    w = csv.writer(open(csvfile,"w"))
+    for row in final_rows:
+        w.writerow(row)
+
 URL = 'http://www.barchart.com/stocks/signals/top100?mode=I&view=main'
 page = requests.get(URL)
 text_of_page = str(page.text)
@@ -41,8 +75,8 @@ for stock in name_list:
 currentTime = str(datetime.datetime.now())
 day = currentTime[0:10]
 time = currentTime[11:16]
-
-csv_name = day + "_portfolio.csv"
+hour = int(currentTime[11:13])
+csv_name = "/home/pi/" + day + "_portfolio.csv"
 
 w = csv.writer(open(csv_name, "w"))
 w.writerow(["Buy threshold = $" + str(buy_threshold)])
@@ -52,5 +86,9 @@ for row in portfolio:
     w.writerow(row)
 
 w.writerow(["Investment", investment, time + " Net Profit", net_profit])
+
+while hour < 17:
+    time.sleep(3600)
+    update(csv_name)
 
 
